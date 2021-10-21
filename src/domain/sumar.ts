009ac86ca2a1140b603f938b62e96c8fb896cc2a
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-21 14:10:10 -0500

    Se llama una funcion desde la clase sumar para que realiza el redondeo del numero cuando son bastantes decimales

--- src/domain/sumar.ts
+++ src/domain/sumar.ts
@@ -1,5 +1,7 @@
 import { IValorNumeros } from "../interface/IValorNumeros"
+import { redonderNumero } from "../redondearNumero";
 
-export const sumar = (valores: IValorNumeros ): Number  => {    
-   return (valores.numero1.valueOf() + valores.numero2.valueOf())
-}
+export const sumar = (valores: IValorNumeros ): Number  => {   
+   const {valor1,valor2} = redonderNumero(valores);
+   return valor1.valueOf() + valor2.valueOf()
+};
