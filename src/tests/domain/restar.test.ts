e46888f80c0de54ff6ce58c395341644b2befbe3
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-21 11:41:49 -0500

    Se adiciona un test a la prueba unitaria de la resta

--- src/tests/domain/restar.test.ts
+++ src/tests/domain/restar.test.ts
@@ -9,5 +9,9 @@ describe('Debe restar', () => {
     test('debe restar valores decimales', () => {
         expect(restar({ numero1: 3.6002, numero2: 3.4})).toBe(0.20020000000000016);
     });
+
+    test('debe restar 2 valores en miles', () => {
+        expect(restar({ numero1: (1000000000e20), numero2: (2000000000e20) })).toBe(-1e+29);
+    });
       
 });
