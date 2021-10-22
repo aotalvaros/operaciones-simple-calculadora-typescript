67e13632d275a8396c2aefbfae3a848af1b289ab
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-22 15:33:06 -0500

    Se adiciona pruebas al archivo redondearADosDecimales

--- src/tests/utils/redondearADosDecimales.test.ts
+++ src/tests/utils/redondearADosDecimales.test.ts
@@ -2,12 +2,19 @@ import { redondearADosDecimales } from "../../utils/redondearADosDecimales";
 
 describe('Debe redondearADosDecimales', () => {
 
-    test('debe la funcion redondearADosDecimales retornar un valor con dos decimales', () => {
+    test('debe redondear a dos decimales', () => {
         expect(redondearADosDecimales(4.67983333)).toBe(4.68);
     });
 
-    test('debe la funcion redondearADosDecimales retornar un valor', () => {
+    test('debe retornar un numero entero, en caso de no tener decimales', () => {
         expect(redondearADosDecimales(4)).toBe(4);
     });
-    
+
+    test('debe redondear a dos decimales por encima, cuando el tercer decimal es mayor a cuatro', () => {
+        expect(redondearADosDecimales(4.6759)).toBe(4.68);
+    }); 
+
+    test('no debe redondear a dos decimales cuando el tercer decimal es menor a cinco', () => {
+        expect(redondearADosDecimales(4.674)).toBe(4.67);
+    });     
 });
