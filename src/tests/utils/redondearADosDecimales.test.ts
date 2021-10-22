21bbca44edc1a32f17025845dc84ea9803b83fb4
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-22 10:35:38 -0500

    Se cambia las pruebas para la funcion redondearADosNumeros

--- src/tests/utils/redondearADosDecimales.test.ts
+++ src/tests/utils/redondearADosDecimales.test.ts
@@ -1,21 +1,13 @@
-import { mockFunction } from "../../helpers/JestHelpers";
 import { redondearADosDecimales } from "../../utils/redondearADosDecimales";
 
-jest.mock('../../utils/redondearADosDecimales', () => ({    
-    redondearADosDecimales: jest.fn()
-}));
-
 describe('Debe redondearADosDecimales', () => {
-    const redondearADosDecimalesMock = mockFunction(redondearADosDecimales);
-     
-    test('debe la funcion redondearADosDecimales ser llamada', () => {
-        redondearADosDecimalesMock(2)
-        expect(redondearADosDecimalesMock).toHaveBeenCalled()
-    });
 
     test('debe la funcion redondearADosDecimales retornar un valor con dos decimales', () => {
-        redondearADosDecimalesMock.mockReturnValue(4.67);
-        expect(redondearADosDecimalesMock(4.67983333)).toBe(4.67);
+        expect(redondearADosDecimales(4.67983333)).toBe(4.68);
     });
-       
+
+    test('debe la funcion redondearADosDecimales retornar un valor', () => {
+        expect(redondearADosDecimales(4)).toBe(4);
+    });
+    
 });
