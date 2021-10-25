867f21d6911e9d1346dc8b93e7d30416363135ab
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-25 14:28:28 -0500

    Se crea un contenedor donde se renderiza la calculadora

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,15 +1,36 @@
 import React, { ChangeEvent, useState } from 'react'
+import { dividir } from '../domain/calculadora/dividir';
+import { multiplicar } from '../domain/calculadora/multiplicar';
+import { restar } from '../domain/calculadora/restar';
 import { sumar } from '../domain/calculadora/sumar';
+import { IValorNumeros } from '../interface/IValorNumeros';
 
 export const Calculadora = () => {
-    const [inputOneState, setInputOne] = useState(0);
-    const [inputTwoState, setInputTwo] = useState(0);       
-    const [inputResultado, setInputResultado] = useState(0);
+    const [inputOneState, setInputOne] = useState<number>(0);
+    const [inputTwoState, setInputTwo] = useState<number>(0);       
+    const [inputResultado, setInputResultado] = useState<Number | String>(0);
 
-    const performAddition = (): void =>{
-        setInputResultado( sumar({ numero1: inputOneState, numero2: inputTwoState}));
+    const valores: IValorNumeros = {
+        numero1: inputOneState,
+        numero2: inputTwoState
+    };
+
+    const performAddition = (): void => {
+        setInputResultado(sumar(valores));
+    };
+
+    const performSubtraction = (): void => {
+        setInputResultado(restar(valores));
+    };
+
+    const performMultiplication = (): void => {
+        setInputResultado(multiplicar(valores))
     };
     
+    const performDivision = (): void => {
+        setInputResultado(dividir(valores));
+    }; 
+
     const handleOnChangeInputOne = ({ target }: ChangeEvent<HTMLInputElement>): void => {
         const {value} = target; 
         setInputOne(Number.parseInt(value));
@@ -18,15 +39,13 @@ export const Calculadora = () => {
         const {value} = target; 
         setInputTwo(Number.parseInt(value));
     };
-    
-        
 
     return (
         <>
             <div>           
                 <input 
                     onChange={handleOnChangeInputOne}            
-                    id='inputObtienePrimerNumero'
+                    id='inputObtienePrimerNumero'                     
                     />
                 <input 
                     onChange={handleOnChangeInputTwo}            
@@ -37,11 +56,20 @@ export const Calculadora = () => {
                 <button id='buttonHacerSumar' onClick={performAddition}>
                     Sumar
                 </button>
+                <button id='buttonHacerResta' onClick={performSubtraction}>
+                    Restar
+                </button>
+                <button id='buttonHacerMultiplicacion' onClick={performMultiplication}>
+                    Multiplicar
+                </button>
+                <button id='buttonHacerDivision' onClick={performDivision}>
+                    Dividir
+                </button>               
             </div>
-            <div>
-                <input
-                    value={inputResultado} 
-                    id='InputMostrarResultado'/> 
+            <div>               
+                 <p id='InputMostrarResultado'> 
+                    {inputResultado} 
+                </p>                                                         
             </div>
         </>
     );
