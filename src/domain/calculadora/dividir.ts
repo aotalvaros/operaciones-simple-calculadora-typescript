a25d8a15e23f510acf943093d9db1f8d6318e6f6
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-25 16:46:13 -0500

    Se tipa la funcion dividir donde regresa un IResultadoDivision

--- src/domain/calculadora/dividir.ts
+++ src/domain/calculadora/dividir.ts
@@ -1,10 +1,17 @@
+import { IResultadoDivision } from "../../interface/IResultadoDivision";
 import { IValorNumeros } from "../../interface/IValorNumeros";
 import { redondearADosDecimales } from "../../utils/redondearADosDecimales";
 
-export const dividir = (valores: IValorNumeros) => {   
+export const dividir = (valores: IValorNumeros): IResultadoDivision => {   
+    let resultadoDivision: IResultadoDivision = {
+        resultado: 0,
+        error: ''
+    }; 
     if (valores.numero2 === 0) {
-        return 'La division por 0 no esta difinida';
-    };     
-    return redondearADosDecimales(valores.numero1) 
-            / redondearADosDecimales(valores.numero2);           
+        resultadoDivision.error = 'La division por 0 no esta difinida';
+    } else {
+        resultadoDivision.resultado = redondearADosDecimales(valores.numero1) 
+        / redondearADosDecimales(valores.numero2);
+    };
+    return resultadoDivision;           
 };
