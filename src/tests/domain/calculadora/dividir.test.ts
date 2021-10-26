e931c0154fc35c7421003fdd1861cb4de5c58015
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-26 07:20:02 -0500

    Se modifica la prueba de la division ya que se tipa que va retornar la funcion

--- src/tests/domain/calculadora/dividir.test.ts
+++ src/tests/domain/calculadora/dividir.test.ts
@@ -6,27 +6,28 @@ jest.mock("../../../utils/redondearADosDecimales");
 
 describe('Debe dividir', () => {
     const redondearADosDecimalesMock = mockFunction(redondearADosDecimales);
+
     
     test('debe dividir 2 valores positivos', () => {
         redondearADosDecimalesMock.mockReturnValueOnce(9);
-        redondearADosDecimalesMock.mockReturnValueOnce(2);  
-        expect(dividir({ numero1: 9, numero2: 2})).toBe(4.5);
+        redondearADosDecimalesMock.mockReturnValueOnce(2);
+        expect(dividir({numero1: 9,numero2: 2}).resultado).toBe(4.5);
     });
 
     test('debe dividir 1 numero negativo y otro positivo', () => {  
         redondearADosDecimalesMock.mockReturnValueOnce(-3);
         redondearADosDecimalesMock.mockReturnValueOnce(3);      
-        expect( dividir({ numero1: -3, numero2: 3})).toBe(-1)
+        expect( dividir({ numero1: -3, numero2: 3}).resultado).toBe(-1)
     });
 
     test('debe dividir valores decimales', () => {
         redondearADosDecimalesMock.mockReturnValueOnce(5.61);
         redondearADosDecimalesMock.mockReturnValueOnce(3.74);       
-        expect( dividir({ numero1: 5.605678, numero2: 3.744567})).toBe(1.5);       
+        expect( dividir({ numero1: 5.605678, numero2: 3.744567}).resultado).toBe(1.5);       
      });
 
      test('debe mostrar un error cuando se divide entre 0', () => {
-        expect(dividir({ numero1: 3, numero2: 0})).toBe('La division por 0 no esta difinida');
+        expect(dividir({ numero1: 3, numero2: 0}).error).toBe('La division por 0 no esta difinida');
      });    
     
 });
