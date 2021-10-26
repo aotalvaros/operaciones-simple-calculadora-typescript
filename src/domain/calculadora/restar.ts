1a117cc0a6615e3f4232e15f322a1c7396e40a34
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-26 07:07:16 -0500

    Se cambia el tipado de la funcion en las operaciones matematicas por un number primitivo

--- src/domain/calculadora/restar.ts
+++ src/domain/calculadora/restar.ts
@@ -2,7 +2,7 @@ import { IValorNumeros } from "../../interface/IValorNumeros";
 import { redondearADosDecimales } from "../../utils/redondearADosDecimales";
 
 
-export const restar = (valores: IValorNumeros ): Number => {
+export const restar = (valores: IValorNumeros ): number => {
     return redondearADosDecimales(valores.numero1) 
          - redondearADosDecimales(valores.numero2);
 };
