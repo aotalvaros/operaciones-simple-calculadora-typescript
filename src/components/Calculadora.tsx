3c9529ef9a8a57dd0353747298421771a46bbdbf
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-26 10:23:52 -0500

    En la funcion division se crea una variable de tipo IRusultadoDIvision,  donde se hace la destructuracion y se guarad en una estado, tambien se le mostrara en un parrafo el resultado que traiga cada operacion.

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -3,32 +3,39 @@ import { dividir } from '../domain/calculadora/dividir';
 import { multiplicar } from '../domain/calculadora/multiplicar';
 import { restar } from '../domain/calculadora/restar';
 import { sumar } from '../domain/calculadora/sumar';
+import { IResultadoDivision } from '../interface/IResultadoDivision';
 import { IValorNumeros } from '../interface/IValorNumeros';
 
 export const Calculadora = () => {
     const [inputOneState, setInputOne] = useState<number>(0);
     const [inputTwoState, setInputTwo] = useState<number>(0);       
-    const [inputResultado, setInputResultado] = useState<Number | String>(0);
+    const [inputResultado, setInputResultado] = useState<number>(0);
+    const [error, setError] = useState<string>('');
 
     const valores: IValorNumeros = {
         numero1: inputOneState,
         numero2: inputTwoState
-    };
+    }; 
 
     const performAddition = (): void => {
         setInputResultado(sumar(valores));
+        limpiarError();
     };
 
     const performSubtraction = (): void => {
         setInputResultado(restar(valores));
+        limpiarError();
     };
 
     const performMultiplication = (): void => {
-        setInputResultado(multiplicar(valores))
+        setInputResultado(multiplicar(valores));
+        limpiarError();
     };
-    
+
     const performDivision = (): void => {
-        setInputResultado(dividir(valores));
+        const { resultado, error }: IResultadoDivision = dividir(valores);       
+        setInputResultado(resultado);
+        setError(error);      
     }; 
 
     const handleOnChangeInputOne = ({ target }: ChangeEvent<HTMLInputElement>): void => {
@@ -40,14 +47,22 @@ export const Calculadora = () => {
         setInputTwo(Number.parseInt(value));
     };
 
+    const limpiarError = (): void => {
+        setError('');
+    };
+
     return (
         <>
             <div>           
                 <input 
+                    type='number'
+                    placeholder='0'
                     onChange={handleOnChangeInputOne}            
                     id='inputObtienePrimerNumero'                     
                     />
                 <input 
+                    type='number'
+                    placeholder='0'
                     onChange={handleOnChangeInputTwo}            
                     id='inputObtieneSegundoNumero'
                     />
@@ -66,10 +81,13 @@ export const Calculadora = () => {
                     Dividir
                 </button>               
             </div>
-            <div>               
-                 <p id='InputMostrarResultado'> 
-                    {inputResultado} 
-                </p>                                                         
+            <div>              
+                <p id='pMostrarResultado'> 
+                    {(isNaN(inputResultado) ? 0 : inputResultado)} 
+                </p>
+                {   error !== '' &&
+                    <p id='pMostarError' style = {{color:'red'}}>{ error }</p>                                                                        
+                }                          
             </div>
         </>
     );
