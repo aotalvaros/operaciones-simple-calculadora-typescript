a28632bbde1f5566fc4e894338d6a38cdce580b2
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-26 22:33:14 -0500

    Se importa el estilo al index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Calculadora } from './components/Calculadora';
+import './style/index.css'
 
 ReactDOM.render(
   <Calculadora/>,
